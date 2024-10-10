3f6c98f8cf5d62fd0cf6e610a60be8329ba06fba
LunaKamituki <163498223+LunaKamituki@users.noreply.github.com>
2024-10-10 19:50:04 +0900

    test

--- update_instance.py
+++ update_instance.py
@@ -1,8 +1,11 @@
 #! python3
+# update_instance.py
 
 import requests
 import ast
 import json
+
+# TODO:並行処理等で効率化
 
 def fetch(urlpath, apis):
     success_apis = []
@@ -14,4 +17,26 @@ def fetch(urlpath, apis):
                 success_apis.append(api)
         except:
             pass
-        return success_apis
+    return success_apis
+
+    
+with open('./instances_list.txt') as f:
+    apis_list = ast.literal_eval(f.read())
+
+videos = fetch('/videos/shs0rAiwsGQ', apis_list)
+search = fetch('/search?q=test&page=1&hl=ja', apis_list)
+channel = fetch('/channles/@cosmobsp', apis_list)
+comment = fetch('/comments/shs0rAiwsGQ', apis_list)
+
+
+with open('./videos_apis', mode="w") as f:
+    f.write(list(set(videos)))
+
+with open('./search_apis', mode="w") as f:
+    f.write(list(set(search)))
+
+with open('./channel_apis', mode="w") as f:
+    f.write(list(set(channel)))
+
+with open('./comment_apis', mode="w") as f:
+    f.write(list(set(comment)))
